ce6b455ef6613d172be3071fe5b81c2d8abfa0a2
Nethsara Prabash <138272058+nethsaraPrabash@users.noreply.github.com>
2024-08-02 18:12:05 +0530

    initial commit java file

---
+++ Java/src/QUEUE/Node.java
@@ -0,0 +1,17 @@
+package QUEUE;
+
+public class Node {
+
+    Node next;
+    String[] patient;
+
+    public Node(String[] patient)
+    {
+        this.next = null;
+        this.patient = patient;
+    }
+
+
+
+
+}
